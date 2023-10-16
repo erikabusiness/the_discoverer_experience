eaaf99255e5a992e1251254a7d3568753a16e1f2
Erika Alves <erikaalves.m.silva@gmail.com>
2023-10-16 13:38:43 -0300

    Data.js reposto

---
+++ site/web/constants/data.js
@@ -0,0 +1,202 @@
+export const navLinks = [
+    {
+        text: "home",
+        page: "index",
+    },
+    {
+        text: "destinos",
+        page: "destinos",
+    },
+    {
+        text: "promoções",
+        page: "promoçoes",
+    },
+    {
+        text: "contato",
+        page: "contato",
+    },
+]
+export const cardHome = [
+    {
+        title:"Dubai",
+        imgname: "dubai",
+        imgnum: 1        
+    }, 
+    {
+        title:"Rio de janeiro",
+        imgname: "rio",
+        imgnum: 2
+        
+    }, 
+    {
+        title:"Disney",
+        imgname: "disney",
+        imgnum: 3
+    },
+    {
+        title:"Maldivas",
+        imgname: "maldivas",
+        imgnum: 4
+    },
+    {
+        title:"Mykonos",
+        imgname: "mykonos",
+        imgnum: 5
+    },
+    {
+        title:"Toronto",
+        imgname: "toronto",
+        imgnum: 6
+    }
+]
+export const btnsHome = [
+    {
+        classBs: "primary",
+        text: "Clube The Discoverer"
+    },
+    {
+        classBs:"warning",
+        text: "Disney"
+    },
+    {
+        classBs : 'primary',
+        text: 'Aluguel de casas'
+    },
+    {
+        classBs : 'danger',
+        text: 'App The Discoverer'
+    },
+    {
+        classBs : 'dark',
+        text: 'Ingressos'
+    },
+    {
+        classBs : 'danger',
+        text: 'Promoções'
+    },
+    {
+        classBs : 'success',
+        text: 'Seguro Viagem'
+    },
+    {
+        classBs : 'success',
+        text: 'Parcerias'
+    },
+    {
+        classBs : 'warning',
+        text: 'Vale-Viagem'
+    },
+    {
+        classBs : 'info',
+        text: 'Sobre'
+    },
+    
+    
+]
+export const cardsDestinos = [
+    {
+        img: "paris_poster",
+        title:"Paris" ,
+        text:"Descubra a mágica e o encanto da icônica capital francesa com nossos empolgantes pacotes de viagem para Paris! Com suas elegantes avenidas, arquitetura deslumbrante e uma riqueza de cultura e história, Paris é uma experiência inesquecível para todos os viajantes."
+    },
+    {
+        img: "orlando_resorts",
+        title:"Orlando Resorts" ,
+        text:"Bem-vindo ao destino dos sonhos para sua próxima aventura! Nossos pacotes exclusivos para Orlando Resorts oferecem uma experiência única para toda a família. Deixe-se envolver pela magia dos parques temáticos famosos mundialmente, como a Disney World e a Universal Studios."
+    },
+    {
+        img: "myanmar",
+        title:"Myanmar" ,
+        text:"Bem-vindo a Myanmar, um tesouro escondido no coração da Ásia. De paisagens exuberantes a uma rica herança cultural, Myanmar é um destino mágico que cativa os viajantes com sua autenticidade e espiritualidade. Explore os templos majestosos e pagodes dourados que pontuam a paisagem, incluindo a icônica Pagoda Shwedagon em Yangon. "
+    }
+]
+export const pacotes = [
+    {
+        type: "nacional",
+        img: "rio",
+        title: "Rio de Janeiro: Descubra a cidade maravilhosa!",
+        days: "5 dias",
+        services: "Aéreo e hotel inclusos",
+        numOfPeople: "2 pessoas e uma criança",
+        text: "Explore o encanto do Rio de Janeiro: praias deslumbrantes, pontos turísticos icônicos e cultura vibrante. Reserve agora e viva momentos inesquecíveis! 🏖️🌴✨",
+        oldPrice: "1500",
+        newPrice: "1000"
+    }, 
+    {
+        type: "nacional",
+        img: "bahia",
+        title: "Encantos de Salvador na Bahia",
+        days: "7 dias",
+        services: "Translado e hotel inclusos",
+        numOfPeople: "2 pessoas",
+        text: "Conheça praias deslumbrantes, a rica história cultural e a energia contagiante da cidade. Desfrute de uma viagem inesquecível com música, gastronomia e paisagens encantadoras.",
+        oldPrice: "R$ 1500",
+        newPrice: "R$ 1000"
+    }, 
+    {
+        type: "nacional",
+        img: "cruzeiro",
+        title: "Navegue pela Costa Brasileira: Cruzeiro Inesquecível!",
+        days: "7 dias",
+        services: "Translado e hotel inclusos",
+        numOfPeople: "2 pessoas",
+        text: "Embarque em uma aventura única e deslumbre-se com a beleza da costa brasileira. Conheça destinos paradisíacos, praias deslumbrantes e paisagens de tirar o fôlego. Viva momentos de pura magia em alto mar. Reserve agora e permita-se essa experiência incomparável! ⚓🛳️🏖️",
+        oldPrice: "1500",
+        newPrice: "1000"
+    }, 
+    {
+        type: "nacional",
+        img: "chapada",
+        title: "Chapada Diamantina: Uma Jornada Inesquecível!",
+        days: "5 dias",
+        services: "Translado, aéreo e hotel inclusos",
+        numOfPeople: "2 pessoas",
+        text: "Explore a beleza selvagem da Chapada Diamantina. Cânions majestosos, cachoeiras deslumbrantes e trilhas fascinantes aguardam por você. Permita-se essa experiência única em meio à natureza exuberante. Reserve agora e embarque nessa aventura inesquecível!",
+        oldPrice: "1500",
+        newPrice: "1000"
+    }, 
+    {
+        type: "internacional",
+        img: "pexels-pixabay-161853",
+        title: "Paris, França - A Cidade do Amor",
+        days: "5 dias",
+        services: "Aéreo e hotel inclusos",
+        numOfPeople: "2 pessoas e uma criança",
+        text: "Explore a beleza romântica de Paris, a capital francesa. Visite a icônica Torre Eiffel, desfrute de deliciosos croissants em charmosas cafeterias e caminhe pelas margens do rio Sena. Uma cidade cheia de cultura, arte e romance espera por você em Paris!✨",
+        oldPrice: "1500",
+        newPrice: "1000"
+    }, 
+    {
+        type: "internacional",
+        img: "toquio",
+        title: "Tóquio, Japão - O Futuro e a Tradição Colidem",
+        days: "7 dias",
+        services: "Translado e hotel inclusos",
+        numOfPeople: "2 pessoas",
+        text: "Mergulhe na cultura japonesa e modernidade futurística em Tóquio. Descubra templos ancestrais, desfrute de pratos de sushi frescos e explore os bairros movimentados da cidade. Tóquio é uma metrópole fascinante que combina tradição e inovação de forma única. 🏖️🎶🌞</p>",
+        oldPrice: "1500",
+        newPrice: "1000"
+    }, 
+    {
+        type: "internacional",
+        img: "sydney",
+        title: "Sydney, Austrália - Onde Praias e Natureza Se Unem",
+        days: "7 dias",
+        services: "Translado e hotel inclusos",
+        numOfPeople: "2 pessoas",
+        text: "Conheça a espetacular Sydney com suas praias deslumbrantes, como a famosa Bondi Beach. Aprecie a impressionante Ópera de Sydney e encante-se com a beleza da Baía de Sydney. Esta cidade australiana oferece uma mistura perfeita entre vida urbana e natureza exuberante. ⚓🛳️🏖️",
+        oldPrice: "1500",
+        newPrice: "1000"
+    }, 
+    {
+        type: "internacional",
+        img: "ny",
+        title: "Nova York, Estados Unidos - A Cidade que Nunca Dorme",
+        days: "5 dias",
+        services: "Translado, aéreo e hotel inclusos",
+        numOfPeople: "2 pessoas",
+        text: "Viva a energia contagiante de Nova York, uma metrópole vibrante e cosmopolita. Explore arranha-céus imponentes, assista a shows da Broadway e passeie pelo Central Park. A cidade oferece uma diversidade cultural e experiências únicas que a tornam um destino inesquecível.",
+        oldPrice: "1500",
+        newPrice: "1000"
+    }, 
+]
